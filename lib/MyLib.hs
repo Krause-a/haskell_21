fb8edb029c32c7b3baeb357b76a8b7327cb729b8
Krause-a <xander876@gmail.com>
2024-02-19 08:11:31 +0000

    day 2

--- lib/MyLib.hs
+++ lib/MyLib.hs
@@ -6,13 +6,22 @@ import Prelude hiding (compare)
 
 daySelector :: Int -> String -> String
 daySelector d | d == 1 = solve_day_1
-              | d == 2 = (++ "48484")
+              | d == 2 = solve_day_2
               | otherwise = id
+
+
+solve_day_2 :: String -> String
+solve_day_2 s = solve_day_2_part_1 s ++ "\n" ++ solve_day_2_part_2 s
+
+solve_day_2_part_1 :: String -> String
+solve_day_2_part_1 = show . uncurry (*) . foldl submarineFlatRoute (0, 0) . map words . lines
+
+solve_day_2_part_2 :: String -> String
+solve_day_2_part_2 = show . (\(_, x, y) -> x * y) . foldl submarineAimRoute (0, 0, 0) . map words . lines
 
 
 solve_day_1 :: String -> String
 solve_day_1 s = solve_day_1_part_1 s ++ "\n" ++ solve_day_1_part_2 s
-
 
 solve_day_1_part_1 :: String -> String
 solve_day_1_part_1  = show . sum . map compare . windows 2 . toIntList . splitAll '\n'
@@ -20,6 +29,17 @@ solve_day_1_part_1  = show . sum . map compare . windows 2 . toIntList . splitAl
 solve_day_1_part_2 :: String -> String
 solve_day_1_part_2  = show . sum . map compare . windows 2 . map sum . windows 3 . toIntList . splitAll '\n'
 
+submarineAimRoute :: (Int, Int, Int) -> [String] -> (Int, Int, Int)
+submarineAimRoute (aim, x, y) [dir, disStr]
+    | dir == "forward" = (aim, dis + x, dis * aim + y)
+    | dir == "down" = (aim + dis, x, y)
+    | dir == "up" = (aim - dis, x, y)
+    where dis = read disStr
+
+submarineFlatRoute :: (Int, Int) -> [String] -> (Int, Int)
+submarineFlatRoute (x, y) [dir, dis] | dir == "forward" = (x + read dis, y)
+                                 | dir == "down" = (x, y + (read dis)) 
+                                 | dir == "up" = (x, y - (read dis)) 
 
 merge :: (String, String) -> String
 merge = uncurry (++)
