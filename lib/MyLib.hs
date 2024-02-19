242b50f3e1a3b9d47563eb85fb4639864e5dacea
Krause-a <xander876@gmail.com>
2024-02-19 22:48:56 +0000

    Day 3 part 1

--- lib/MyLib.hs
+++ lib/MyLib.hs
@@ -1,14 +1,31 @@
 module MyLib (daySelector) where
 import Data.List (intercalate)
 import Text.Read (readMaybe)
-import Data.Maybe (fromMaybe)
-import Prelude hiding (compare)
+import Debug.Trace
 
 daySelector :: Int -> String -> String
 daySelector d | d == 1 = solve_day_1
               | d == 2 = solve_day_2
+              | d == 3 = solve_day_3
               | otherwise = id
 
+solve_day_3 :: String -> String
+solve_day_3 s = solve_day_3_part_1 s ++ "\n" ++ solve_day_3_part_2 s
+
+solve_day_3_part_1 :: String -> String
+solve_day_3_part_1 xs = let intList = map listCharsToInt $ lines xs
+                            commonBits = map (\x -> if x > 0 then 1 else 0) $ foldl1 (zipWith commonBitStep) intList
+                            gamma = powerTwo commonBits
+                            epsilon = powerTwo $ invertBits commonBits
+                            in show (gamma * epsilon)
+
+
+
+solve_day_3_part_2 :: String -> String
+solve_day_3_part_2 xs = let intList = map listCharsToInt $ lines xs
+                            oxygen = bitCriteria True intList
+                            co2 = bitCriteria False intList
+                            in show $ oxygen ++ 9 : co2
 
 solve_day_2 :: String -> String
 solve_day_2 s = solve_day_2_part_1 s ++ "\n" ++ solve_day_2_part_2 s
@@ -24,10 +41,45 @@ solve_day_1 :: String -> String
 solve_day_1 s = solve_day_1_part_1 s ++ "\n" ++ solve_day_1_part_2 s
 
 solve_day_1_part_1 :: String -> String
-solve_day_1_part_1  = show . sum . map compare . windows 2 . toIntList . splitAll '\n'
+solve_day_1_part_1  = show . sum . map compareDayOne . windows 2 . toIntList . splitAll '\n'
 
 solve_day_1_part_2 :: String -> String
-solve_day_1_part_2  = show . sum . map compare . windows 2 . map sum . windows 3 . toIntList . splitAll '\n'
+solve_day_1_part_2  = show . sum . map compareDayOne . windows 2 . map sum . windows 3 . toIntList . splitAll '\n'
+
+
+bitCriteria :: Bool -> [[Int]] -> [Int]
+bitCriteria _ [x] = x
+bitCriteria wantCommon xs = let
+    filtered = filter filter' (trace (show xs) xs)
+    reducedColumns = map tail filtered
+    -- The common bit is a lie. It works when there is a most common bit. When they are tied, I cannot assume the actual bit is the same as the tie breaker bit... Don't forget about trace. It is great!
+    in commonBit : bitCriteria wantCommon reducedColumns
+    where 
+        columnSum = foldl (\acc x -> acc + if x > 0 then 1 else -1) 0 $ map head xs
+        commonBit = case compare columnSum 0 of 
+            LT -> 0
+            EQ -> if wantCommon then 1 else 0
+            GT -> 1
+        filterCommon = (\x -> head x == commonBit)
+        filterUncommon = (\x -> head x /= commonBit)
+        filter' = if wantCommon then filterCommon else filterUncommon
+
+invertBits :: [Int] -> [Int]
+invertBits = map (\x -> if x == 1 then 0 else 1)
+
+powerTwo :: [Int] -> Int
+powerTwo xs = sum $ zipWith (*) (reverse xs) (map (2^) [0,1..])
+
+toBinary :: [Int] -> String
+toBinary xs = foldl (\acc x -> acc ++ if x > 0 then "1" else "0") "" xs
+
+commonBitStep :: Int -> Int -> Int
+commonBitStep acc x = acc + step x
+    where step = \x -> if x == 1 then 1 else -1
+
+listCharsToInt :: String -> [Int]
+listCharsToInt [] = []
+listCharsToInt (x:xs) = read [x] : listCharsToInt xs
 
 submarineAimRoute :: (Int, Int, Int) -> [String] -> (Int, Int, Int)
 submarineAimRoute (aim, x, y) [dir, disStr]
@@ -50,8 +102,8 @@ toIntList (x:xs) = case readMaybe x of
                 Just a -> a : toIntList xs
                 Nothing -> toIntList xs
 
-compare :: [Int] -> Int
-compare [x, y] | x < y = 1
+compareDayOne :: [Int] -> Int
+compareDayOne [x, y] | x < y = 1
                | otherwise = 0
 
 windows :: Int -> [a] -> [[a]]
